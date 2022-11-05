1b9859ac1c3651d38eb412ea18707c5abb969ae0
abris <abrahamzsombornagy@gmail.com>
2022-11-05 15:01:36 +0100

    add verbose comments to explain code to users without programming background

--- content-script.js
+++ content-script.js
@@ -2,7 +2,7 @@
 // Any other facebook page, eg. the user page gives an empty list, so no action is taken.
 // I only let this script to be initialized on other facebook pages too, as there is no real
 // redirection between facebook pages, just the url changes which doesn't trigger the browser
-// to initialize it on the home page when it was navigated from other facebook pages.
+// to initialize the script on the home page when it was navigated from other facebook pages.
 function getArticles() {
     for (const h3 of document.getElementsByTagName("h3")) {
         if (h3.innerHTML === "Hírfolyambejegyzések") {
