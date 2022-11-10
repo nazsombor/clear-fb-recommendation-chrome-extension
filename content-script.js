0f78f62548c513f5a69decf2f6496555eff889d6
Abraham Nagy <nazsombor@protonmail.com>
2022-11-10 12:36:49 +0100

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -8,7 +8,6 @@ var Type = {
 // Every second run the following review:
 setInterval(() => {
     for (article of feed()) {
-        
         // To achieve a better performance every article is given with a custom HTML attribute 'reviewed' that
         // is set to "true". Any article that has this attribute is skipped, so they only categroized once.
         if (article.getAttribute('reviewed')) continue
